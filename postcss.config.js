89a0bd27c05db48ed3178d820a474620baa134e7
Daruma_Labo <kohey.deluxe.08@gmail.com>
2025-08-13 16:30:21 +0900

    Auto-commit at Wed Aug 13 16:30:21 JST 2025

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: [
-    tailwind(),
-    require('autoprefixer'),
-  ],
-};
+  plugins: [tailwind(), autoprefixer()],
+}
 
 
